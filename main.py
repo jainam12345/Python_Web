b27fdd1e32a5f02efe54eace3e1f01729a988b4a
jainam sharma <100946736+jainam12345@users.noreply.github.com>
2023-02-24 20:43:25 +0000

    Added api route and also added dynamic web page.

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+  "index": "1",
+  "title": "Computer Engineer",
+  "Salary": "$100,000",
+  "location": "Kampala, Uganda"
+}, {
+  "index": "2",
+  "title": "Backend Engineer",
+  "Salary": "$120,000",
+  "location": "California, USA"
+}, {
+  "index": "3",
+  "title": "Front-End Developer",
+  "Salary": "$90,000",
+  "location": "Remote"
+}, {
+  "index": "4",
+  "title": "Data Analyst",
+  "Salary": "$140,000",
+  "location": "Ahmedabad, India"
+}]
+
+
 @app.route("/")
 def hello():
-  return render_template('index.html')
+  return render_template('index.html', jobs=JOBS)
 
+@app.route("/jobs")
+def jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
